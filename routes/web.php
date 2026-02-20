3b48519e62ff414d541f84139a11b3340fd19788
Rijo Johny <rijomjohnyk@gmail.com>
2026-02-20 15:27:12 +0000

    Force HTTPS scheme in production

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ContactController;
-/*
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/_clear', function () {
+    \Artisan::call('optimize:clear');
+    return response()->json(['status' => 'cleared']);
 });
-*/
+
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/services', [PageController::class, 'services'])->name('services');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
